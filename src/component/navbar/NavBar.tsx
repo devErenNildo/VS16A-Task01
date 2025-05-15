9e4dcec2a370050e092688d50f6fd7aa5007f121
eren <dev.erenildo@gmail.com>
2025-05-15 10:01:26 -0300

    botao menu responsivo

--- src/component/navbar/NavBar.tsx
+++ src/component/navbar/NavBar.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
+"use client"
+
+import React, { useState } from 'react';
+import { IoMenuSharp, IoClose } from "react-icons/io5";
 import styles from './navbar.module.scss';
 import Link from 'next/link';
 
-export default function Navbar () {
+export default function Navbar() {
+
+  const [ativo, setAtivo] = useState(false);
+  const alterarEstado = (): void => {
+    setAtivo(!ativo);
+  }
 
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navLinks}>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/quemsomos">Quem Somos</Link></li>
-        <li><Link href="/contato">Contato</Link></li>
-      </ul>
-    </nav>
+    <>
+      <nav
+        className={`${styles.navbar} ${ativo ? styles.menuAtivo : ""}`}
+        onClick={alterarEstado}
+      >
+        <ul className={styles.navLinks}>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/quemsomos">Quem Somos</Link></li>
+          <li><Link href="/contato">Contato</Link></li>
+        </ul>
+      </nav>
+
+      {
+        ativo
+          ? <IoClose className={styles.icon} onClick={alterarEstado} />
+          : <IoMenuSharp className={styles.icon} onClick={alterarEstado} />
+      }
+
+    </>
   );
 };
