8d303106cfbc3c230e86d442585d9fa8f0060550
eren <dev.erenildo@gmail.com>
2025-05-16 12:15:14 -0300

    bug context

--- src/app/logado/page.tsx
+++ src/app/logado/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useAuth } from "@/context/AuthContext";
 
-
-const { logout } = useAuth();
 export default function Logado() {
+    const { logout } = useAuth();
+
     return (
         <>
             <h2>Ainda estamos trabalhando nisso, obrigado pela confiança...</h2>
             <h3>Por enquanto, faça o Logout no botão abaixo</h3>
-            <button onClick={() => logout()} style={{}}> Sair</button>
+            <button onClick={logout}>Sair</button>
         </>
     );
 }
